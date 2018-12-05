e91697133e41ee44d4d642fa04e51b804685d575
fickrybiliman <bil.iman.fickry@gmail.com>
2018-12-05 13:54:43 +0700

    promise migration add kecamatanID dan kelurahanID di table konstituen

--- migrations/20181128083548-add-kecamatanID-dan-kelurahanID-di-table-konstituen.js
+++ migrations/20181128083548-add-kecamatanID-dan-kelurahanID-di-table-konstituen.js
@@ -9,14 +9,14 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-    return [
+    return Promise.all([
       queryInterface.addColumn('Konstituens', 'kecamatanID', {
         type: Sequelize.INTEGER
       }),
       queryInterface.addColumn('Konstituens', 'kelurahanID', {
         type: Sequelize.INTEGER
       })
-    ];
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
@@ -27,9 +27,9 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-    return [
+    return Promise.all([
       queryInterface.removeColumn('Konstituens', 'kecamatanID'),
       queryInterface.removeColumn('Konstituens', 'kelurahanID')
-    ]; 
+    ]); 
   }
 };
