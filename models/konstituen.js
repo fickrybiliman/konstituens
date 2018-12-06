afd4250218a64d7452ef8edd28e2fa164ed9d1b8
fickrybiliman <bil.iman.fickry@gmail.com>
2018-12-06 13:41:41 +0700

    create hooks before create konstituen name, change to uppercase after white space

--- models/konstituen.js
+++ models/konstituen.js
@@ -14,5 +14,19 @@ module.exports = (sequelize, DataTypes) => {
     Konstituen.belongsTo(sequelize.models.Kecamatan, {foreignKey: 'kecamatanID'}),
     Konstituen.belongsTo(sequelize.models.Kelurahan, {foreignKey: 'kelurahanID'})
   };
+  Konstituen.beforeCreate(konstituen => {
+    let konstituenNama = konstituen.nama[0].toUpperCase();
+    for (let i = 1; i < konstituen.nama.length; i++) {
+      if (konstituen.nama[i] === ' ') {
+        konstituenNama += ' ';
+      } else if (konstituen.nama[i-1] === ' ' || konstituen.nama[i-1] === '.') {
+        konstituenNama += konstituen.nama[i].toUpperCase();
+      } else if (konstituen.nama[i] !== ' ') {
+        konstituenNama += konstituen.nama[i];
+      }
+    }
+    konstituen.nama = konstituenNama;
+    return konstituen;
+  });
   return Konstituen;
 };
